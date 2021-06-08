b8cb41dc1e2d10663023fe5c3181237dba6d7075
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-08 19:31:24 -0300

    Comentarios

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            // Cuidados com os comentarios desnecessarios
+            Console.WriteLine("Codigo claro é sempre melhor!");
+
+            /*
+            *   Esse é um comentario
+            *   de multiplas linhas...
+            */
+            Console.WriteLine("O C# tem o XML Comments");
+        }
+    }
+}
