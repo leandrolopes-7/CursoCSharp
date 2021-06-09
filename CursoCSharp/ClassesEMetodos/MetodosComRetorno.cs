4b6fa47e1d64c3f7aff20a4a7b9a6f9f64bdc545
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-09 19:37:23 -0300

    Metodos com retorno

---
+++ CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class CalculadoraComum
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Dividir(int a, int b){
+            return a / b;
+        }
+    }
+    class MetodosComRetorno
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Digite o primeiro numero: ");
+            int.TryParse(Console.ReadLine(), out int a);
+
+            Console.WriteLine("Digite o segundo numero: ");
+            int.TryParse(Console.ReadLine(), out int b);
+
+            var calculadoraComum = new CalculadoraComum();
+            Console.WriteLine(calculadoraComum.Somar(a, b));
+            Console.WriteLine(calculadoraComum.Subtrair(a, b));
+            Console.WriteLine(calculadoraComum.Dividir(a, b));
+            Console.WriteLine(calculadoraComum.Multiplicar(a, b));
+
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -41,6 +41,7 @@ namespace CursoCSharp {
                 //Classes e Metodos
                 {"Membros - Classes e Metodos", Membros.Executar },
                 {"Construtores - Classes e Metodos", Construtores.Executar },
+                {"Metodos com Retorno - Classes e Metodos", MetodosComRetorno.Executar },
 
             });
 
