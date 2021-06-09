3757e2b2503d6da27d0c988f5686c7d73efc5d13
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-08 23:28:26 -0300

    Conversões

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -15,6 +15,7 @@ namespace CursoCSharp {
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar },
                 {"Lendo Dados - Fundamentos", LendoDados.Executar },
                 {"Formatando Numero - Fundamentos", FormatandoNumero.Executar },
+                {"Conversões - Fundamentos", Conversoes.Executar },
             });
 
             central.SelecionarEExecutar();
