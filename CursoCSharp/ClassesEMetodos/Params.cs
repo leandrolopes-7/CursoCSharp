c744edba8ad45df9d654ba78973566545f3f9d77
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-10 00:02:27 -0300

    Parametros Nomeados

---
+++ CursoCSharp/ClassesEMetodos/Params.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Params
+    {
+
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach(var pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+        }
+
+        public static void Executar()
+        {
+            Recepcionar("Leandro", "Mariana", "Antonio", "Quiteria");
+        }
+    }
+}
