1ccdd426c1f2171cb49406542c6fa0c692b1551e
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-16 14:48:59 -0300

    Coleções Queue

---
+++ CursoCSharp/Colecoes/ColecoesQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace CursoCSharp.Colecoes
+{
+    class ColecoesQueue
+    {
+        public static void Executar()
+        {
+            var fila = new Queue<string>();
+
+            fila.Enqueue("Leandro");
+            fila.Enqueue("Antonio");
+            fila.Enqueue("Quiteria");
+            fila.Enqueue("Mariana");
+
+            Console.WriteLine(fila.Peek());
+            Console.WriteLine(fila.Count);
+
+            Console.WriteLine(fila.Dequeue());
+            Console.WriteLine(fila.Count);
+
+            foreach(var pessoa in fila)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+            var salada = new Queue();
+            salada.Enqueue("Banana");
+            salada.Enqueue(4);
+            salada.Enqueue(false);
+            salada.Enqueue(3.14);
+
+            Console.WriteLine(salada.Contains("banana"));
+            Console.WriteLine(salada.Contains("Banana"));
+
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -63,6 +63,7 @@ namespace CursoCSharp {
                 {"List - Coleções", ColecoesList.Executar },
                 {"ArrayList - Coleções", ColecoesArrayList.Executar },
                 {"Set - Coleções", ColecoesSet.Executar },
+                {"Queue - Coleções", ColecoesQueue.Executar },
 
 
             });
