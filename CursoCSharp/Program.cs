5ca230759599c46a587e617c5253574cda1216de
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-17 13:21:24 -0300

    Encapsulamento

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -72,6 +72,7 @@ namespace CursoCSharp {
                 //OO
                 {"Herança - Orientação a Objeto",Heranca.Executar  },
                 {"Constructor This - Orientação a Objeto",ConstructorThis.Executar  },
+                {"Encapsulamento - Orientação a Objeto", OO.Encapsulamento.Executar },
 
 
             });
---
+++ Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    public class SubCelebridade
+    {
+        // Todos
+        public string InfoPublica = "Tenho um instagram!";
+
+        //Herança
+        protected string CorDoOlho = "Verde";
+
+        //Mesmo projeto (Assembly)
+        internal ulong NumeroCelular = 5511999999999;
+
+        //Herança ou mesmo projeto
+        protected internal string JeitoDeFalar = "Uso Muitas Girias";
+
+        //MEsma class ou herança no mesmo projeto (c# >= 7.2)
+        private protected string SegredoFamilia = "Bla bla";
+
+        //private é o padrão
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("SubCelebridade...");
+
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
